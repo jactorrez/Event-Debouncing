675f1cfa2bc2c35fcfc20c38e7772090bfb83486
Javier Torrez <javierctorrez@gmail.com>
2017-02-09 23:24:40 -0500

    finished implementing throttling functionality

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-function debounce(func, wait = 20, immediate = true) {
+function debounce(func, wait = 15, immediate = true) {
   var timeout;
 
   return function() {
     var context = this, args = arguments;
+    
     var later = function() {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
+
     var callNow = immediate && !timeout;
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
@@ -17,7 +19,23 @@ function debounce(func, wait = 20, immediate = true) {
 const images = document.querySelectorAll(".slide-in");
 
 function checkSlide(e){
-  console.count(e);
+
+  images.forEach((img, i) => {
+    // halfway through the image 
+    const slideInAt = (window.scrollY + window.innerHeight) - (img.height / 2);
+
+    // bottom of the image
+    const imgBottom = img.offsetTop + img.height;
+
+    const isHalfShown = slideInAt > img.offsetTop;
+    const isNotScrolledPast = window.scrollY < imgBottom; 
+
+    if(isHalfShown && isNotScrolledPast){
+      img.classList.add('active');
+    } else {
+      img.classList.remove('active');
+    }
+  });
 }
 
 window.addEventListener("scroll", debounce(checkSlide));
