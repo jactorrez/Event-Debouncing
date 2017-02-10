702a5c6aadc07cd7571eb7e01875fd2370695622
Javier Torrez <javierctorrez@gmail.com>
2017-02-09 21:39:49 -0500

    initial commit and setup

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const images = document.querySelectorAll(".slide-in");
+
+function checkSlide(e){
+  console.count(e);
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
